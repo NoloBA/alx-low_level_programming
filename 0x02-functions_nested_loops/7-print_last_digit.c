8cea0a5098bf4e280c655016b8fc86248bbf39a1
NoloBA <purposedriven.ba@gmail.com>
2023-06-21 11:28:37 -0700

    betty spaces fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,17 @@
 
 /**
  * print_last_digit: print the last digit of a given number
- * @n is number input
+ * @n: is number input
  * Return: last digit number.
  */
-int print_last_digit(int n)
+int print_last_digit (int n)
 {
 int last;
 last = n % 10;
-if(last < 0)
+if (last < 0)
 {
 last = last * -1;
 }
-_putchar(last + '0');
+_putchar (last + '0');
 return (last);
 }
