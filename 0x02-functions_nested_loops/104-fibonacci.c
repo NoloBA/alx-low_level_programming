ddf8a53b47bdae2d2c2977ebab7b94ef22418aaf
NoloBA <purposedriven.ba@gmail.com>
2023-06-21 12:59:43 -0700

    solution fibonacci seq first 98 from 1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,9 +25,9 @@ b_head = b / 1000000000;
 b_tail = b % 1000000000;
 for (; counter < 99; counter++)
 {
-overflow = (a_tail + b_tail) /1000000000;
+overflow = (a_tail + b_tail) / 1000000000;
 sum_tail = (a_tail + b_tail) -(1000000000 * overflow);
-sum_head = (a_head + b_head) + overflow;
+sum_head = (a_head + b_head) +overflow;
 printf(", %lu%lu", sum_head, sum_tail);
 a_head = b_head;
 a_tail = b_tail;
