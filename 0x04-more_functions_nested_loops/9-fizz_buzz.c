fc0f008ef80af9959f1ced68d591d9a834f25567
NoloBA <purposedriven.ba@gmail.com>
2023-06-22 18:52:13 -0700

    fix fizzbuzz solution initialize int to 0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,34 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * FizzBuzz
- *
- * A program that prints the numbers from 1 to 100.
- * Multiples of three print “Fizz” and multiples of five print “Buzz”.
- * For numbers which are multiples of both three and five print “FizzBuzz”.
- */
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i;
-	for (i = 1; i <= 100; i++) {
-        if (i % 15 == 0) {
-            printf("FizzBuzz\n");
-        } else if (i % 3 == 0) {
-            printf("Fizz\n");
-        } else if (i % 5 == 0){
-            printf("Buzz\n");
-        } else {
-            printf("%u\n", i);
-        }
-    }
-    return 0;
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+
+		if ((i % 3) != 0 && (i % 5) != 0)
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
 }
