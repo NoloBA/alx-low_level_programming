3a751142b8c685f098427e6efde43d75ca128948
NoloBA <purposedriven.ba@gmail.com>
2023-06-22 18:26:13 -0700

    fizzbuzz solution

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,20 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i = 0; i <= 100; i++)
-	{
-		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Bizz");
-		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
-	}
-	printf("\n");
-	return (0);
+`int i;
+for (i = 0; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Bizz");
+else
+printf("%i", i);
+if (i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
 }
